b86da765cdfce20ea12f9e22f3a6a1a04ac56a31
Arifin-only <arifin180104@gmail.com>
2024-10-11 01:50:52 +0800

    Create Dengan_For_In.dart

---
+++ Dengan_For_In.dart
@@ -0,0 +1,13 @@
+void main(){
+    
+    var nama = <String>['Upin','Ipin','Apin'];
+    for(var value in nama){
+        print(value);
+    }
+    
+     var namaSet = <String>['Upin','Ipin','Apin'];
+    for(var value in namaSet){
+        print(value);
+    }
+ 
+}
